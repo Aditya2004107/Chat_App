35e6c30cd306f638c58a84054e2f06e93a4ae652
Aditya2004107 <officiallyaditya07@gamil.com>
2025-01-01 15:49:26 +0530

    Chat_App

--- Frontend/src/context/useGetSocketMessage.jsx
+++ Frontend/src/context/useGetSocketMessage.jsx
@@ -1,24 +1,3 @@
-// import { useEffect } from "react";
-// import useConversation from "../statemanage/useConversation.js";
-// import { useSocketContext } from "./SocketContext.jsx";
-// import sound from "../assets/noti.mp3";
-// const useGetSocketMessage = () => {
-//   const { socket } = useSocketContext();
-//   const { messages, setMessages } = useConversation();
-
-//   useEffect(() => {
-//     socket.on("newMessage", (newmessage) => {
-//       const notification = new Audio(sound);
-//       notification.play();
-//       setMessages([...messages, newmessage]);
-//     });
-//     return () => {
-//       socket.off("newMessage");
-//     };
-//   }, [socket, messages, setMessages]);
-// };
-
-// export default useGetSocketMessage;
 import { useEffect } from "react";
 import useConversation from "../statemanage/useConversation.js";
 import { useSocketContext } from "./SocketContext.jsx";
